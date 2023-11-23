4cf23027cec70d999d5b0a2dae6b54276eb91a5a
정송이 <pso7364@nate.com>
2023-11-23 14:01:41 +0900

    매 시각 정각이되면 리프레쉬(새로고침)

--- dashboard/src/main/webapp/resources/js/main/main.js
+++ dashboard/src/main/webapp/resources/js/main/main.js
@@ -1,20 +1,32 @@
 /* 정각이 되면 페이지 리프레쉬 시작 */
-function refreshAtMidnight() {
+/* 매 시간 정각마다 페이지 리프레쉬 시작 */
+function refreshAtTopOfHour() {
+    // 현재 시간 정보 가져오기
     var now = new Date();
-    var millisTillMidnight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 24, 0, 0, 0) - now;
-    if (millisTillMidnight < 0) {
-        millisTillMidnight += 86400000; // 다음날 정각까지의 시간 (24시간 = 86400000 밀리초)
+    
+    // 다음 정각 시간 설정
+    var nextHour = new Date(now);
+    nextHour.setHours(now.getHours() + 1);
+    nextHour.setMinutes(0);
+    nextHour.setSeconds(0);
+    nextHour.setMilliseconds(0);
+    
+    // 다음 정각까지의 시간 계산
+    var millisTillNextHour = nextHour - now;
+    
+    // 정각이 이미 지났으면 다음 시간으로 설정
+    if (millisTillNextHour < 0) {
+        millisTillNextHour += 3600000; // 1시간 = 3600000 밀리초
     }
     
-    setTimeout(function () {
+    // 타이머 설정하여 매 정각마다 새로 고침
+    setInterval(function () {
         location.reload(true); // true 파라미터는 캐시를 무시하고 새로고침
-    }, millisTillMidnight);
+    }, millisTillNextHour);
 }
 
-// 페이지 로드 시 실행
-document.addEventListener("DOMContentLoaded", function () {
-    refreshAtMidnight();
-});
+// 페이지 로드 후 함수 호출
+refreshAtTopOfHour();
 /* 정각이 되면 페이지 리프레쉬 끝 */
 
 
